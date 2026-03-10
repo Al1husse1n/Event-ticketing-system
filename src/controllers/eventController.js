4d5ce89bf679b15b6523ea5e6301750bc2f6c53d
Ali Hussein <ah0979075074@gmail.com>
2026-03-10 18:38:32 +0300

    search feature

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,9 +1,9 @@
 import { prisma } from "../config/db";
-import {userRole} from "..."
+
 
 const createEvent = async(req, res) => {    
     try{
-        const {name, totalSeats, eventDate} = req.body;
+        const {name, totalSeats, eventDate, category} = req.body;
         const user = req.user;  
         if(user.role !== "ORGANIZER"){
             return res.status(401).json({error:"Only Organizers are allowed to create an Event"})
@@ -16,6 +16,7 @@ const createEvent = async(req, res) => {
                 totalSeats: parseInt(totalSeats),
                 availableSeats: parseInt(totalSeats),
                 eventDate: new Date(eventDate),
+                category: category
             },
         });
 
@@ -37,7 +38,7 @@ const createEvent = async(req, res) => {
 
 const updateEvent = async(req, res) => {
     try{
-        const {name, totalSeats, eventDate} = req.query;
+        const {name, totalSeats, eventDate, category} = req.body;
         const user = req.user;
         const event_id = req.params.event_id;
 
@@ -49,8 +50,8 @@ const updateEvent = async(req, res) => {
             return res.status(404).json({error: "The event no longer exists"});
         }
 
-        if(user.id !== event.userId || user.role !== "ORGANIZER"){
-            return res.status(401).json({error:"Only event Organizers are allowed to update an Event"})
+        if(user.role !== "ORGANIZER" || user.id !== event.userId){
+            return res.status(403).json({error:"Only event Organizers are allowed to update an Event"})
         }
 
         const updatedEvent = await prisma.event.update({
@@ -59,7 +60,9 @@ const updateEvent = async(req, res) => {
             data:{
                 name: name ? name : event.name,
                 totalSeats: totalSeats ? parseInt(totalSeats) : event.totalSeats,
-                eventDate: eventDate ? new Date(eventDate) : event.eventDate
+                availableSeats: totalSeats ? Math.max(0,parseInt(totalSeats) - event.availableSeats) : event.availableSeats,
+                eventDate: eventDate ? new Date(eventDate) : event.eventDate,
+                category: category? category: event.category,   
             }
         });
 
@@ -89,8 +92,8 @@ const deleteEvent = async(req, res) =>{
             return res.status(404).json({error: "Event is no longer available"});
         }
 
-        if(user.id !== event.userId || user.role !== "ORGANIZER"){
-            return res.status(401).json({error:"Only event Organizers are allowed to delete"})
+        if(user.role !== "ORGANIZER" || user.id !== event.userId){
+            return res.status(403).json({error:"Only event Organizers are allowed to delete"})
         }
 
         const deletedEvent = await prisma.event.update({
@@ -104,7 +107,7 @@ const deleteEvent = async(req, res) =>{
             status:"success",
             data:{
                 deletedEvent
-            }
+            }       
         })
     }
 
@@ -114,4 +117,59 @@ const deleteEvent = async(req, res) =>{
     }
 }
 
-export {createEvent, updateEvent, deleteEvent}
+const searchEvent = async(req,res) =>{
+    try{
+        const {page, limit, category, upcoming, userId, name, date} = req.query;
+        const pageNumber = parseInt(page) || 1;
+        const pageSize = parseInt(limit) || 10;
+
+        const where = {
+                    ...(upcoming === "true" && {eventDate: {gte: new Date()}}),
+                    ...(category && {category: category.toUpperCase()}),
+                    ...(userId  && {userId: userId}),
+                    ...(name  && {name: {contains: name, mode:"insensitive"}}),
+                    ...(date && {eventDate: {
+                                        gte: new Date(date),
+                                        lt: new Date(new Date(date).setDate(new Date(date).getDate() + 1))
+                                    }}),
+                    
+                    isDelete: false,
+                }
+
+        const [events, total] = await Promise.all([
+            prisma.event.findMany({
+            where,  
+            skip: (pageNumber - 1) * pageSize,
+            take: pageSize,
+            orderBy: {
+                eventDate: "asc"
+            }
+            }),
+
+            prisma.event.count({where})
+        ]);
+
+        const totalPages = Math.ceil(total/pageSize);
+
+        return res.status(200).json({
+            status:"success",
+            data:{
+                events,
+                totalPages,
+                currentPage: pageNumber,
+                pageSize,
+                hasNextPage: pageNumber < totalPages,
+                hasPreviousPage: pageNumber > 1
+            }
+        });
+            
+
+    }
+
+    catch(error){
+        console.error(error);
+        return res.status(500).json({error:"Failed to search events"})
+    }
+}
+
+export {createEvent, updateEvent, deleteEvent, searchEvent};
--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import { config } from 'dotenv'
+import cookieParser from "cookie-parser";
 import authRoutes from './routes/authRoutes.js'
 
 
-config()
+config()            
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));           
+app.use(express.urlencoded({extended:true}));  
+app.use(cookieParser());         
 
 app.use('/auth', authRoutes);
 
