f2b0935c4563bea792d841485ba34345c318f366
Ali Hussein <ah0979075074@gmail.com>
2026-03-14 02:19:03 +0300

    add validator middleware

---
+++ src/middlewares/validateRequest.js
@@ -0,0 +1,31 @@
+export const validateRequest = (schema) => {
+  return (req, res, next) => {
+    const result = schema.safeParse({
+      body: req.body,
+      params: req.params,
+      query: req.query
+    });
+
+    if (!result.success) {
+      const formatted = result.error.format();
+
+      const flatErrors = Object.values(formatted)
+        .flat()
+        .filter(Boolean)
+        .map((err) => err._errors)
+        .flat();
+
+      console.log(flatErrors);
+
+      return res.status(400).json({
+        message: flatErrors.join(", ")
+      });
+    }
+
+    if (result.data.body) req.body = result.data.body;
+    if (result.data.params) req.params = result.data.params;
+    if (result.data.query) req.query = result.data.query;
+
+    next();
+  };
+};
