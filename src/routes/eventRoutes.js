6e5fe2a2ef218cad5f9f1045e66ea553fb30971e
Ali Hussein <ah0979075074@gmail.com>
2026-03-14 02:24:09 +0300

    add zod validation

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import {createEvent, updateEvent, deleteEvent, viewEvents, viewEventReservation} from "../controllers/eventController.js";
+import { createEventSchema, updateEventSchema, deleteEventSchema, viewEventReservationSchema } from '../validators/eventValidators.js';
+import { validateRequest } from '../middlewares/validateRequest.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.post("/createEvent", createEvent);
-router.patch("/updateEvent", updateEvent);
-router.delete("/deleteEvent", deleteEvent);
+router.post("/createEvent", validateRequest(createEventSchema), createEvent);
+router.patch("/updateEvent", validateRequest(updateEventSchema), updateEvent);
+router.delete("/deleteEvent", validateRequest(deleteEventSchema), deleteEvent);
 router.get("/", viewEvents);                
-router.get("/:event_id/eventReservation", viewEventReservation);
+router.get("/:event_id/eventReservation", validateRequest(viewEventReservationSchema), viewEventReservation);
 
 export default router;
 
