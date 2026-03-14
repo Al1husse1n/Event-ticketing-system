401adea661cc467158b6db0bbbe564924063a7e0
Ali Hussein <ah0979075074@gmail.com>
2026-03-14 05:50:03 +0300

    add validators and routers

--- src/server.js
+++ src/server.js
@@ -1,18 +1,40 @@
 import express from 'express'
 import { config } from 'dotenv'
 import cookieParser from "cookie-parser";
-import authRoutes from './routes/authRoutes.js'
+import helmet from "helmet";
+import cors from "cors";
+import authRoutes from './routes/authRoutes.js';
+import eventRoutes from './routes/eventRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
+import profileRoutes from './routes/profileRoutes.js';
+import searhEventRoutes from './routes/searchEventRoutes.js';
 
 
-config()            
+
+config()     
+
+if (!process.env.JWT_SECRET) {
+  console.error("JWT_SECRET missing");
+  process.exit(1);
+}
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));  
-app.use(cookieParser());         
+app.use(cookieParser()); 
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+        
 
 app.use('/auth', authRoutes);
+app.use('/event', eventRoutes);
+app.use('/payment', paymentRoutes);
+app.use('/profile', profileRoutes);
+app.use('/searchevent', searhEventRoutes);
 
 const PORT = process.env.PORT || 5001;
 
@@ -27,7 +49,6 @@ server.on("error", (err) => {
 process.on("unhandledRejection", (err) =>{
     console.error("Unhandled rejection: ", err);
     server.close(async() =>{
-        await disconnectDB();
         process.exit(1);
     });
 });
@@ -35,14 +56,12 @@ process.on("unhandledRejection", (err) =>{
 
 process.on("uncaughtException", async(err) => {
     console.error("uncaught Exception:", err);
-    await disconnectDB();
     process.exit(1);
 });
 
 process.on("SIGTERM", async()=>{
     console.log("SIGTERM received, shutting down gracefully");
     server.close(async () =>{       
-        await disconnectDB();
         process.exit(0);
     });
 });
